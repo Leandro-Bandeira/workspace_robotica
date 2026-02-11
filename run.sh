4f0ea8c5dc4eb862061e8b6cdf495daa91d6b4c6
Leandro <leoviolinist22@gmail.com>
2026-02-11 12:29:34 -0300

    Adicionando sleep entre as exec

--- run.sh
+++ run.sh
@@ -15,31 +15,37 @@ tmux new-session -d -s $SESSION -n description
 # Aba 0 - Robot Description
 tmux send-keys -t $SESSION:0 \
   "source install/setup.bash && ros2 launch robotics_class robot_description.launch.py" C-m
+sleep 1 # Aguarda 1 segundo
 
 # Aba 1 - Simulation World
 tmux new-window -t $SESSION -n simulation
 tmux send-keys -t $SESSION:1 \
   "source install/setup.bash && ros2 launch robotics_class simulation_world.launch.py" C-m
+sleep 5 # RECOMENDAÇÃO: O Gazebo é pesado, 1s geralmente é pouco. Coloquei 5s aqui.
 
 # Aba 2 - EKF
 tmux new-window -t $SESSION -n ekf
 tmux send-keys -t $SESSION:2 \
   "source install/setup.bash && ros2 launch robotics_class ekf.launch.py" C-m
+sleep 1
 
 # Aba 3 - RViz
 tmux new-window -t $SESSION -n rviz
 tmux send-keys -t $SESSION:3 \
   "source install/setup.bash && ros2 launch robotics_class rviz.launch.py" C-m
+sleep 1
 
 # Aba 4 - Teleop
 tmux new-window -t $SESSION -n teleop
 tmux send-keys -t $SESSION:4 \
   "source install/setup.bash && ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args --remap cmd_vel:=jetauto/cmd_vel" C-m
+sleep 1
 
 # Aba 5 - NAV2 (executa antes do SLAM)
 tmux new-window -t $SESSION -n nav2
 tmux send-keys -t $SESSION:5 \
   "source install/setup.bash && ros2 launch nav2_bringup navigation_launch.py use_sim_time:=true" C-m
+sleep 3 # O Nav2 demora um pouco para carregar os costmaps
 
 # Aba 6 - SLAM (última)
 tmux new-window -t $SESSION -n slam
