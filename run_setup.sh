90de47f3fc1c6eb0837ad8a282d7b5604afa35c8
Leandro <leoviolinist22@gmail.com>
2026-02-11 12:12:03 -0300

    Adicionando dependendo do xacro

--- run_setup.sh
+++ run_setup.sh
@@ -5,4 +5,5 @@ sudo apt install ros-humble-navigation2
 sudo apt install ros-humble-nav2-bringup
 rosdep install --from-paths src --ignore-src -r -y
 sudo apt install ros-humble-gazebo-ros-pkgs
+sudo apt install ros-humble-xacro
 colcon build
